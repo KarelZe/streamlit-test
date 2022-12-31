73857e057a75db2e88aa331245fc5cfd7a335ffb
Markus Bilz <github@markusbilz.com>
2022-12-31 15:30:45 +0100

    Sort imports 🔢

--- myapp.py
+++ myapp.py
@@ -1,8 +1,8 @@
-import yfinance as yf
 from datetime import datetime as dt
-import pytz
 
+import pytz
 import streamlit as st
+import yfinance as yf
 
 tz = pytz.timezone("America/New_York")
 start = tz.localize(dt(2013,1,1))
@@ -12,6 +12,3 @@ tickers = "MA,V,AMZN,JPM,BA".split(",")
 df = yf.download(tickers,start, end, auto_adjust=True)['Close']
 
 st.table(df.head())
-
-
-#st.global_settings.timezone 
